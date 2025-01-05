fe41966bd47b1a7044937aa61c5ffde9e5ae06e8
anirudh-korapole <anirudh.korapole@gmail.com>
2025-01-06 00:56:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Sandbox(),  
+  ));
+
+}
+
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sandbox"),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: 100,
+            color: Colors.red,
+            child: const Text("One"),
+          ),
+          Container(
+            width: 200,
+            color: Colors.green,
+            child: const Text("Two"),
+          ),
+          Container(
+            width: 300,
+            color: Colors.blue,
+            child: const Text("Three"),
+          ),
+        ],
+      ) 
+    );
+  }
+}
