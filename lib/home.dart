a0aa89eb6c39499f66a02cda9f3e32963d959b74
anirudh-korapole <anirudh.korapole@gmail.com>
2025-01-06 01:56:18 +0530

    styles added to home page

--- lib/home.dart
+++ lib/home.dart
@@ -6,11 +6,28 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My coffee Id'),
+        title: const Text('My Coffee ID', style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        )),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: const Text("Hello"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text("How I like my coffee"),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const Text("Coffee Prefs"),
+          ),
+        ],
+      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Sandbox(),  
+    home: Home(),  
   ));
 
 }
